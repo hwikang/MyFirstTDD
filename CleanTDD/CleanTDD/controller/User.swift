b73ae48a61bd603d62ea3a5ed8784e1a31df9e9c
simon <simon.kang@luxrobo.com>
2022-05-01 19:22:20 +0900

    test

---
+++ CleanTDD/CleanTDD/controller/User.swift
@@ -0,0 +1,15 @@
+//
+//  Use.swift
+//  CleanTDD
+//
+//  Created by 슈퍼 on 2022/05/01.
+//
+
+import Foundation
+
+
+extension User {
+    func signUp() -> User{
+        return createUser(name:"Simon")
+    }
+}
---
+++ CleanTDD/CleanTDD/domain/Entity.swift
@@ -0,0 +1,13 @@
+//
+//  Entity.swift
+//  CleanTDD
+//
+//  Created by 슈퍼 on 2022/05/01.
+//
+
+import Foundation
+
+
+struct User {
+    let name : String
+}
